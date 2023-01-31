e325ef35aeb499cdcb8845d1d09bc05dd7ed155f
Yamato Hokari <yahokari@c1r22s11.42tokyo.jp>
2023-01-31 18:18:22 +0900

    01 done

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,11 +1,56 @@
 #include	"Account.hpp"
+#include	<iostream>
 
 Account::Account(int initial_deposit)
 {
-	;
+	makeDeposit(initial_deposit);
 }
 
 Account::~Account(void)
 {
 	;
+}
+
+int	Account::getNbAccounts(void)
+{
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (Account::_totalNbWithdrawals);
+}
+
+void	Account::t::_displayTimestamp(void)
+{
+	;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	Account::_displayTimestamp();
+	std::cout << " accounts:" << Account::getNbAccounts();
+	std::cout << ";total:" << Account::getTotalAmount();
+	std::cout << ";deposits:" << Account::getNbDeposits();
+	std::cout << ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
+}
+
+void	makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+}
+
+void	makeWithdrawal(int withdrawal)
+{
+	this->_amount -= withdrawal;
 }
