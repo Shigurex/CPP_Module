968dc1f21a7550eed48646bbcf63e2cd358fb194
Yamato Hokari <yamatohokari@gmail.com>
2023-02-27 14:30:26 +0900

    ex03 modified

--- cpp01/ex06/Harl.hpp
+++ cpp01/ex06/Harl.hpp
@@ -10,7 +10,7 @@
 # define INFO_MESSAGE "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n"
 # define WARNING_MESSAGE "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\n"
 # define ERROR_MESSAGE "This is unacceptable! I want to speak to the manager now.\n"
-# define INVALID_MESSAGE "[ Probably complaining about insignificant problems ]\n"
+# define INVALID_MESSAGE "[ Probably complaining about insignificant problems ]"
 
 class Harl
 {
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -1,13 +1,13 @@
 #include	"Harl.hpp"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	Harl	harl;
 
-	harl.complain("INVALID");
-	harl.complain("DEBUG");
-	harl.complain("INFO");
-	harl.complain("WARNING");
-	harl.complain("ERROR");
+	if (argc != 2) {
+		std::cerr << "Invalid input!" << std::endl;
+		return (1);
+	}
+	harl.complain(argv[1]);
 	return (0);
 }
