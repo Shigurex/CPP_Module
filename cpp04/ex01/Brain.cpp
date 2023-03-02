55ddfb50d112eda53239dee33fd0b4f03905abf3
Yamato Hokari <yamatohokari@gmail.com>
2023-03-02 15:14:52 +0900

    updated to new

--- cpp04/ex01/Brain.cpp
+++ cpp04/ex01/Brain.cpp
@@ -2,7 +2,10 @@
 
 Brain::Brain()
 {
-	;
+	for (int i = 0; i < SIZE; i++)
+		this->ideas[i] = "";
+	index = 0;
+	is_full = false;
 }
 
 Brain::Brain(const Brain& obj)
@@ -20,4 +23,23 @@ Brain&	Brain::operator=(const Brain& rhs)
 	for (int i = 0; i < SIZE; i++)
 		this->ideas[i] = rhs.ideas[i];
 	return (*this);
+}
+
+std::string	Brain::get_idea(const int index) const
+{
+	if (index < 0 || SIZE <= index || (this->is_full == false && index >= this->index)) {
+		std::cerr << "Invalid index" << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+	return (this->ideas[index]);
+}
+
+void	Brain::set_idea(std::string idea)
+{
+	this->ideas[index] = idea;
+	index++;
+	if (index == SIZE) {
+		index = 0;
+		is_full = true;
+	}
 }
--- cpp04/ex01/Brain.hpp
+++ cpp04/ex01/Brain.hpp
@@ -2,6 +2,8 @@
 # define BRAIN_HPP
 
 # include	<string>
+# include	<iostream>
+# include	<cstdlib>
 
 # define SIZE 100
 
@@ -9,11 +11,15 @@ class Brain
 {
 private:
 	std::string	ideas[SIZE];
+	int			index;
+	bool		is_full;
 public:
 	Brain();
 	Brain(const Brain& obj);
 	~Brain();
-	Brain&	operator=(const Brain& rhs);
+	Brain&		operator=(const Brain& rhs);
+	std::string	get_idea(const int index) const;
+	void		set_idea(std::string idea);
 };
 
 #endif
